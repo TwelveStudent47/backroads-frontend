6c7524ecbc35a54792377cff47a201aefedd8558
Kevin Laczkó <kevin.laczko@pomscloud.ie>
2026-03-29 16:03:43 +0200

    feat: implemented workflow for services

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -1,5 +1,6 @@
 import "../index.css"
 import Title from "./Title";
+import { services } from "../data";
 
 export default function Services() {
     return (
@@ -9,38 +10,17 @@ export default function Services() {
                 <Title title="our" subTitle="services"/>
                 </div>
                 <div className="section-center services-center">
-                <article className="service">
-                    <span className="service-icon"><i className="fas fa-wallet fa-fw"></i></span>
-                    <div className="service-info">
-                    <h4 className="service-title">saving money</h4>
-                    <p className="service-text">
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                        Asperiores, officia.
-                    </p>
-                    </div>
-                </article>
-
-                <article className="service">
-                    <span className="service-icon"><i className="fas fa-tree fa-fw"></i></span>
-                    <div className="service-info">
-                    <h4 className="service-title">endless hiking</h4>
-                    <p className="service-text">
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                        Asperiores, officia.
-                    </p>
-                    </div>
-                </article>
-
-                <article className="service">
-                    <span className="service-icon"><i className="fas fa-socks fa-fw"></i></span>
-                    <div className="service-info">
-                    <h4 className="service-title">amazing comfort</h4>
-                    <p className="service-text">
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                        Asperiores, officia.
-                    </p>
-                    </div>
-                </article>
+                    {services.map((service) => {
+                        return (
+                            <article className="service" key={service.id}>
+                                <span className="service-icon"><i className={service.icon}></i></span>
+                                <div className="service-info">
+                                <h4 className="service-title">{service.title}</h4>
+                                <p className="service-text">{service.description}</p>
+                            </div>
+                        </article>
+                        );
+                    })}
                 </div>
             </section>
         </div>
--- src/data.js
+++ src/data.js
@@ -38,3 +38,23 @@ export const socialLinks = [
         href: "https://www.twitter.com"
     }
 ]
+
+export const services = [
+    {
+        id: 1,
+        icon: "fas fa-wallet fa-fw",
+        title: "saving money",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, officia."
+    },
+    {
+        id: 2,
+        icon: "fas fa-tree fa-fw",
+        title: "endless hiking",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, officia."
+    },{
+        id: 3,
+        icon: "fas fa-socks fa-fw",
+        title: "amazing comfort",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, officia."
+    }
+]
