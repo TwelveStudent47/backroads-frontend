2e2f18b33d7162eafc34a43e7ab5bf4b6c4f39e8
Kevin Laczkó <kevin.laczko@pomscloud.ie>
2026-03-29 15:51:48 +0200

    feat: implemented data.js for better workflow

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,6 @@
 import "../index.css";
 import logo from "../images/logo.svg";
+import { pageLinks, socialLinks } from "../data";
 
 export default function Navbar() {
   return (
@@ -13,63 +14,31 @@ export default function Navbar() {
             </button>
           </div>
           <ul className="nav-links" id="nav-links">
-            <li>
-              <a href="#home" className="nav-link">
-                {" "}
-                home{" "}
-              </a>
-            </li>
-
-            <li>
-              <a href="#about" className="nav-link">
-                {" "}
-                about{" "}
-              </a>
-            </li>
-
-            <li>
-              <a href="#services" className="nav-link">
-                {" "}
-                services{" "}
-              </a>
-            </li>
-
-            <li>
-              <a href="#tours" className="nav-link">
-                {" "}
-                tours
-              </a>
-            </li>
+            {pageLinks.map((link) => {
+                return (
+                    <li key={link.id}>
+                        <a href={link.href} className="nav-link">
+                            {link.value}
+                        </a>
+                    </li>
+                )
+            })}
           </ul>
 
           <ul className="nav-icons">
-            <li>
-              <a
-                href="https://www.twitter.com"
-                target="_blank"
-                className="nav-icon"
-              >
-                <i className="fab fa-facebook"></i>
-              </a>
-            </li>
-            <li>
-              <a
-                href="https://www.twitter.com"
-                target="_blank"
-                className="nav-icon"
-              >
-                <i className="fab fa-twitter"></i>
-              </a>
-            </li>
-            <li>
-              <a
-                href="https://www.twitter.com"
-                target="_blank"
-                className="nav-icon"
-              >
-                <i className="fab fa-squarespace"></i>
-              </a>
-            </li>
+            {socialLinks.map((link) => {
+                return (
+                    <li key={link.id}>
+                        <a
+                            href={link.href}
+                            target="_blank"
+                            className="nav-icon"
+                        >
+                            <i className={link.class}></i>
+                        </a>
+                    </li>
+                );
+            })}
           </ul>
         </div>
       </nav>
---
+++ src/data.js
@@ -0,0 +1,40 @@
+export const pageLinks = [
+    {
+        id: 1,
+        href: "#home",
+        value: "home"
+    }, 
+    {
+        id: 2,
+        href: "#about",
+        value: "about"
+    },
+    {
+        id: 3,
+        href: "#services",
+        value: "services"
+    },
+    {
+        id: 4,
+        href: "#tours",
+        value: "tours"
+    }, 
+];
+
+export const socialLinks = [
+    {
+        id: 1,
+        class: "fab fa-facebook",
+        href: "https://www.twitter.com"
+    },
+    {
+        id: 2,
+        class: "fab fa-twitter",
+        href: "https://www.twitter.com"
+    },
+    {
+        id: 3,
+        class: "fab fa-squarespace",
+        href: "https://www.twitter.com"
+    }
+]
