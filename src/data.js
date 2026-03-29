54727e690094f692ba8b35aa6c70771ee4889be0
Kevin Laczkó <kevin.laczko@pomscloud.ie>
2026-03-29 16:17:26 +0200

    feat: implemented workflow for tours

--- src/data.js
+++ src/data.js
@@ -1,3 +1,8 @@
+import image1 from "./images/tour-1.jpeg"
+import image2 from "./images/tour-2.jpeg"
+import image3 from "./images/tour-3.jpeg"
+import image4 from "./images/tour-4.jpeg"
+
 export const pageLinks = [
     {
         id: 1,
@@ -56,5 +61,52 @@ export const services = [
         icon: "fas fa-socks fa-fw",
         title: "amazing comfort",
         description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, officia."
+    }
+]
+
+export const tours = [
+    {
+        id: 1,
+        image: image1,
+        date: "august 26th, 2020",
+        title: "Tibet Adventure",
+        description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque vitae tempore voluptatum maxime reprehenderit eum quod exercitationem fugit, qui corporis",
+        icon: "fas fa-map",
+        place: "china",
+        dayCount: 6,
+        price: 2100
+    },
+    {
+        id: 2,
+        image: image2,
+        date: "october 1th, 2020",
+        title: "best of java",
+        description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque vitae tempore voluptatum maxime reprehenderit eum quod exercitationem fugit, qui corporis",
+        icon: "fas fa-map",
+        place: "indonesia",
+        dayCount: 11,
+        price: 1400
+    },
+    {
+        id: 3,
+        image: image3,
+        date: "september 15th, 2020",
+        title: "explore hong kong",
+        description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque vitae tempore voluptatum maxime reprehenderit eum quod exercitationem fugit, qui corporis",
+        icon: "fas fa-map",
+        place: "hong kong",
+        dayCount: 8,
+        price: 5000
+    },
+    {
+        id: 4,
+        image: image4,
+        date: "december 5th, 2020",
+        title: "kenya highlights",
+        description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque vitae tempore voluptatum maxime reprehenderit eum quod exercitationem fugit, qui corporis",
+        icon: "fas fa-map",
+        place: "kenya",
+        dayCount: 20,
+        price: 3300
     }
 ]
